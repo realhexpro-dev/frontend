4e8194b774563acb426983a7023765166e9276cc
realhexpro-dev <realhexpro@gmail.com>
2026-01-21 13:19:38 +0500

    Set default language to TJ

--- lib/i18n/language-context.tsx
+++ lib/i18n/language-context.tsx
@@ -15,7 +15,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined)
 
 export function LanguageProvider({ children }: { children: React.ReactNode }) {
-  const [language, setLanguageState] = useState<Language>("en")
+  const [language, setLanguageState] = useState<Language>("tj")
 
   useEffect(() => {
     const saved = localStorage.getItem("agrolife-language") as Language
